fb8647540031ba2f4d9bdab28945bd64762482d0
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-02 14:05:54 -0500

    refactored sum method

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -9,11 +9,20 @@ end
 def sum(numbers = [])
 	if numbers == []
 		0
-	elsif (numbers[0] == 1) && (numbers[1] == 3) && (numbers[2] == 5) && (numbers[3] == 7) && (numbers[4] == 9)
-		25
-	elsif (numbers[0] == 7) && (numbers[1] == 11)
-		18
-	elsif	numbers[0] == 7
-		7
+	else
+		total = 0
+		numbers.each do |num|
+			total = total + num
+		end
+		total.to_i
 	end
+	# if numbers == []
+	# 	0
+	# elsif (numbers[0] == 1) && (numbers[1] == 3) && (numbers[2] == 5) && (numbers[3] == 7) && (numbers[4] == 9)
+	# 	25
+	# elsif (numbers[0] == 7) && (numbers[1] == 11)
+	# 	18
+	# elsif	numbers[0] == 7
+	# 	7
+	# end
 end
