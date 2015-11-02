3e68e8da503524305597bc78251a38f5173b0133
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-02 14:21:06 -0500

    refactored echo and shout methods

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -2,17 +2,19 @@
 
 
 def echo(say)
-	if say == "hello"
-		"hello"
-	elsif say == "bye"
-		"bye"
-	end
+	say
+	# if say == "hello"
+	# 	"hello"
+	# elsif say == "bye"
+	# 	"bye"
+	# end
 end
 
 def shout(say)
-	if say == "hello"
-		"HELLO"
-	elsif say == "hello world"
-		"HELLO WORLD"
-	end
+	say.upcase
+	# if say == "hello"
+	# 	"HELLO"
+	# elsif say == "hello world"
+	# 	"HELLO WORLD"
+	# end
 end
