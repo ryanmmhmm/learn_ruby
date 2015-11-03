5b80986ae49b984bfc9c82b7bc23d9f110d1103d
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-03 01:42:42 -0500

    book.rb | all tests are passing

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -10,11 +10,24 @@ class Book
 	def title
 		name = @title.split
 
-		name.each do |name|
-			case name
-			when "the" then "the"
+		name.each_with_index do |word, i|
+			if i == 0
+				case word
+				when ["the"] then "The"
+				else
+					word = word.capitalize!
+				end
 			else
-				name = name.capitalize!
+				case word
+				when "the" then "the"
+				when "in" then "In"
+				when "and" then "And"
+				when "an" then "An"
+				when "a"	then "A"
+				when "of" then "Of"
+				else
+					word = word.capitalize!
+				end
 			end
 		end
 		@title = name.join(' ')
