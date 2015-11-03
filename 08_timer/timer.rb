fe347615f40d4a6e7134be32475f2c92ae6d9475
ryanmmhmm <magowan.ryan@gmail.com>
2015-11-03 17:03:56 -0500

    hacked it, got the algorithm, all tests pass

--- 08_timer/timer.rb
+++ 08_timer/timer.rb
@@ -7,4 +7,17 @@ class Timer
 		@seconds = 0
 	end
 
+	def time_string
+		# the string is made up of 'aa:bb:cc'
+		a = (@seconds / 3600).to_s # hours
+		b = ((@seconds % 3600) / 60).to_s # minutes
+		c = ((@seconds % 3600) % 60).to_s # seconds
+
+		if a.length == 1 then a = a.prepend("0") end
+		if b.length == 1 then b = b.prepend("0") end
+		if c.length == 1 then c = c.prepend("0") end
+
+		@time_string = "#{a}:#{b}:#{c}"
+	end
+
 end
